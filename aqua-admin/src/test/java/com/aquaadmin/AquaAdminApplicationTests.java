b7e9633d2eefc906051b47a87347271f4c0c8530
carava07 <chaitanya_arava@hotmail.com>
2022-01-16 10:56:14 -0600

    fixing unit test

--- aqua-admin/src/test/java/com/aquaadmin/AquaAdminApplicationTests.java
+++ aqua-admin/src/test/java/com/aquaadmin/AquaAdminApplicationTests.java
@@ -1,13 +1,9 @@
 package com.aquaadmin;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AquaAdminApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
