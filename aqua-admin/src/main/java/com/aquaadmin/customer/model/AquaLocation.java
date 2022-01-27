90c51e76f38c8cd532f0b9787961e8d8dd3dbba8
carava07 <chaitanya_arava@hotmail.com>
2022-01-26 21:59:31 -0600

    saving location and pond details

--- aqua-admin/src/main/java/com/aquaadmin/customer/model/AquaLocation.java
+++ aqua-admin/src/main/java/com/aquaadmin/customer/model/AquaLocation.java
@@ -12,6 +12,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -66,6 +67,7 @@ public class AquaLocation implements Serializable{
 	@Column(name="status")
 	private String status;
 	
-	@OneToMany(mappedBy = "aquaLocation", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "location_id")
 	private Set<AquaPonds> aquaPonds;
 }
